0492c62a7b840895eed4aeb5da9e3adfe934fab1
Jason Wu <jasonwupilly1997@gmail.com>
2013-04-17 19:50:53 -0400

    hopefully completely fixed the int limit problem

--- c++/md5/main.cpp
+++ c++/md5/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sstream>
+#include <cmath>
 #include "md5.h"
 using namespace std;
 char chars[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '='};
@@ -13,13 +14,15 @@ int fi = 0;
 int maxChars = 15;
 //unsigned long long int counter = 4294967295; 
 unsigned long long int counter = 0;
+unsigned long long int max_counter = 10000000;
+unsigned long long int distancei = 0; 
+bool start_reached = false; 
+//bool counterplusplus = true; 
 void checkPassword(string password);
 void recurse(int width, int position, string baseString);
-bool reset = false; 
 void decrypt(string input)
 {
 	cout << "Decrypt Input:" << endl << input << endl;
-	//cout << "Charset length:" << strtolen(chars) << endl;
     t = input;
     charsetLength = sizeof(chars);
     cout << "charset length: " << charsetLength << endl;
@@ -44,24 +47,61 @@ string encrypt_md5(string input)
     return (md5(input));
 }
 void checkPassword(string password) {
+	if (counter >= max_counter)
+	{
+		if (start_reached == false)
+		{
+			cout << "max_counter reached start_reached is false - reset" << endl;
+			if (si > 0)
+			{
+				si = si - counter; 
+			}
+			if (fi > 0)
+			{
+				fi = fi - counter; 
+			}
+			counter = 0;
+			checkPassword(password); 
+		}
+		else
+		{
+			cout << "max_counter reached - stopping" << endl;
+			exit(1); 
+		}
+	}
+	else
+	{
 	if (counter >= si)
 	{
-	if (reset == false)
+		//cout << "counter check - " << counter << endl; 
+		if (start_reached == false)
+		{
+			cout << "start_reached = true, counter reset" << endl; 
+			start_reached = true; 	
+			
+			if (si != 0 && fi != 0)
+			{
+				distancei = abs(fi - si) + 1; 	
+				fi = fi - si; 
+			}
+			else if (si == 0 && fi != 0)
+			{
+				distancei = fi + 1; 	
+			}
+			si = 0;
+			cout << "distancei set" << endl << distancei << endl; 
+			counter = 0; 
+			cout << "counter reset" << endl; 
+			cout << counter << endl; 
+			//return; 
+		}
+		//cout << "counter check 2- " << counter << endl; 
+	//if (counter <= fi || fi == 0 && (distancei == 0 || counter <= distancei))
+	if (distancei == 0 || counter <= distancei)
 	{
-		reset = true;
-		counter = 0;
-		cout << "counter reset" << endl; 
-		//si = 0;	
-		fi = fi - si; 
-		cout << "final int set" << endl;
-		si = 0;
-		cout << "start int reset" << endl; 
 		
-	}
-	if (counter <= fi || fi == 0)
-	{
     string md5hash = encrypt_md5(password);
-//cout << md5hash << endl;
+	//cout << counter << ":" << md5hash << endl;
     if (md5hash==t) {
       cout << "match [" << md5hash << "]" << endl;
       cout << "counter - " << counter << endl;
@@ -73,11 +113,15 @@ void checkPassword(string password) {
 	{
 		cout << "reached final index" << endl;
 		cout << "counter - " << counter << endl;
+		cout << distancei << endl; 
+		cout << "password" << endl << password << endl; 
+		cout << "FAIL" << endl; 
 		exit(1);	
 	}
 	}
-	counter++;
-}
+	counter++; 
+	}
+	}
 int main(int argc, char* argv[]) {
 	cout << "# of Parameters\n" << argc - 1 << endl;
 	cout << "Parameters\n";
